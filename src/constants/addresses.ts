7c5f072560b17a243152834e8cacc95a4235791d
ngc40 <ngc40@bfly.space>
2021-11-19 11:13:44 +0800

    update

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -7,15 +7,13 @@ interface IAddresses {
 let STC_MAINNET: IAddresses = {};
 Object.keys(process.env).forEach(key => {
     const addressMatch = key.match(/^REACT_APP_(.*_ADDRESS)$/);
-    if (!addressMatch) return;
+    if (!addressMatch || addressMatch!.length < 1) return;
 
     const addressName = addressMatch[1];
     if (addressName) {
         STC_MAINNET[addressName] = String(process.env[key]);
     }
 });
-
-console.log(process.env);
 
 export const getAddresses = (networkID: number) => {
     if (networkID === Networks.STC) return STC_MAINNET;
